51a87115c120fd8450cb1d45a22233b5ef330650
tkhamthi <40312@prot.ac.th>
2025-03-11 15:10:47 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,9 @@
+
+number = int(input("Please Put Your fucking Number less than 25/n"))
+
+if number >= 25 :
+    print("This Number More than 25 ,are you stupid")
+else :
+    while number <= 25:
+        print(f"Inside the loop, my variable is {number}")
+        number += 1
