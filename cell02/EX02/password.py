32cae008554213456668f725eba1dca54d9a0d29
tkhamthi <40312@prot.ac.th>
2025-03-11 11:50:03 +0700

    Add files via upload

---
+++ cell02/EX02/password.py
@@ -0,0 +1,7 @@
+inp_password = input("Please Enter Your password")
+password = "Pytohon IsAwesome"
+
+if inp_password == password :
+    print("ACCESS GRANTED")
+else :
+    print("ACCESS DENIED")    
